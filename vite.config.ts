058bb0ca39582ad934760722cb8db88831ba064d
(ubuntu) Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-09-16 09:25:21 +0600

    singlefile export works

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import {viteSingleFile} from "vite-plugin-singlefile"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      svelte()
-  ],
+    plugins: [
+        svelte(), viteSingleFile({removeViteModuleLoader: true})],
+    build: {
+        minify: true,
+    },
 })
