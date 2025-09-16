ef8eb3cf1850c4e42aeb4ff5c625187eac7b0e86
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-09-16 23:47:31 +0600

    Justed added all html

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,88 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
 </script>
 
+<header>
+    <div class="container row">
+        <div class="brand">
+            <div class="logo"></div>
+            <h1 class="title">Code Editor</h1>
+        </div>
+        <div class="row">
+            <button id="saveBtn" class="btn secondary" title="Save work locally">
+                <span>Save</span>
+            </button>
+            <button id="loadBtn" class="btn secondary" title="Load from file">
+                <span>Load</span>
+            </button>
+            <input id="openFile" type="file" accept="application/json" hidden>
+        </div>
+    </div>
+</header>
+
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    <aside class="card panel stack">
+        <h2>Task/Assignment</h2>
+        <textarea id="assigment" class="text" placeholder="Write the text description ..."></textarea>
 
-  <div class="card">
-    <Counter />
-  </div>
+        <div class="stack">
+            <label for="textArea">Validation test (JavaScript only - optional)</label>
+            <textarea id="textArea" class="text"
+                      placeholder="Write simple tests to append after the students code to see the solution"></textarea>
+            <div class="muted">
+                Tip:
+                <span class="kbd">Ctrl</span> + <span class="kbd">S</span> to save,
+                <span class="kbd">Ctrl</span> + <span class="kbd">Enter</span> to run.
+            </div>
+        </div>
+    </aside>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+    <section class="stack" id="web-only">
+        <div id="webEditors" class="stack panel card">
+            <div class="row`">
+                <h2>Html/CSS/JS</h2>
+                <button class="btn ok" id="runWeb">Run</button>
+                <button class="btn ok" id="Open Preview">Preview</button>
+            </div>
+        </div>
 
-  <p class="read-the-docs">
-    Yeah baby
-  </p>
+        <div aria-label="web editors" class="tabs" id="webTabs" role="tablist">
+            <button class="tab active" role="tab" aria-selected="true" tabindex="0" data-pane="html">HTML</button>
+            <button class="tab" role="tab" aria-selected="false" tabindex="-1" data-pane="css">CSS</button>
+            <button class="tab" role="tab" aria-selected="false" tabindex="-2" data-pane="js">JS</button>
+        </div>
+
+        <div class="editor-wrap" data-pane="html">
+            <div id="ed_html" class="editor"></div>
+        </div>
+
+
+        <div class="editor-wrap" data-pane="css" hidden>
+            <div id="ed_css" class="editor"></div>
+        </div>
+
+        <div class="editor-wrap" data-pane="js" hidden>
+            <div id="ed_js" class="editor"></div>
+        </div>
+
+        <div class="stack" >
+            <h3>Preview</h3>
+            <iframe id="preview" class="preview" sandbox="allow-scripts allow-same-origin allow-modals allow-forms"></iframe>
+        </div>
+    </section>
+
+    <aside class="card panel stack">
+        <h2>Output</h2>
+        <div id="output" class="out" aria-live="polite"></div>
+        <div class="row">
+            <button class="warn">Run with tests</button>
+            <button class="btn secondary">Clear log</button>
+        </div>
+        <h3>Notes</h3>
+        <ul class="muted">
+            <il>Everything runs inside the browser in a sandbox.</il>
+            <il>You can save the file as Json file and restore it...</il>
+        </ul>
+    </aside>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div class="footer">&copy;2025 IAFahim - All rights reserved.</div>
